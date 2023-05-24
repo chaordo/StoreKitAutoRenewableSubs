087b0204e8102774e6e2f59d919822d4f5938a45
quintus <john@chaordo.com>
2023-05-24 16:07:26 -0600

    Initial Commit

---
+++ StoreKitAutoRenewableSubs/StoreKitAutoRenewableSubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitAutoRenewableSubsApp.swift
+//  StoreKitAutoRenewableSubs
+//
+//  Created by John on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitAutoRenewableSubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
